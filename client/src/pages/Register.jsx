18ac2f1e32c3245733aa5f16197f44dcec9c86ac
Rajarshi Mazumder <raj1156mazumder@gmail.com>
2024-11-05 23:15:45 +0900

    Set up registration

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,44 +1,59 @@
-import React from 'react'
-import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
-import Logo from '../components/Logo';
-import FormRow from '../components/FormRow';
-import {Form, redirect, useNavigation, Link} from 'react-router-dom'; 
-import {customFetch} from '../utils/customFetch';
+import React from "react";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import Logo from "../components/Logo";
+import FormRow from "../components/FormRow";
+import { Form, redirect, useNavigation, Link } from "react-router-dom";
+import { customFetch } from "../utils/customFetch";
+import { toast } from "react-toastify";
 
-export const action = async ({request})=>{
-  const formData=  await request.formData();
-  const data= Object.fromEntries(formData);
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
   console.log(data);
 
-  try{
-    await customFetch.post('/auth/register', data);
-    return redirect("/login");;
-  }
-  catch(error){
+  try {
+    await customFetch.post("/auth/register", data);
+    toast.success("Registration successful");
+    return redirect("/login");
+  } catch (error) {
+    toast.error(error.response?.data?.msg);
     console.log(error);
     return error;
   }
-
-}
+};
 
 const Register = () => {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <Wrapper>
-      <Form method='post'  className='form' action='/register'>
+      <Form method="post" className="form" action="/register">
         <Logo />
         <h4>Register</h4>
-        <FormRow type='text' name='name' defaultValue='john'/>
-        <FormRow type='text' name='lastName' defaultValue='doe' labelText='last name'/>
-        <FormRow type='text' name='location' defaultValue='earth'/>
-        <FormRow type='email' name='email' defaultValue='john@gmail.com'/>
-        <FormRow type='password' name='password' defaultValue='secret123'/>
-        <button type='submit' className='btn btn-clock' >submit</button>
-        <p>Already a  member?
-        <Link to='/login' className='member-btn'>Login</Link>
+        <FormRow type="text" name="name" defaultValue="john" />
+        <FormRow
+          type="text"
+          name="lastName"
+          defaultValue="doe"
+          labelText="last name"
+        />
+        <FormRow type="text" name="location" defaultValue="earth" />
+        <FormRow type="email" name="email" defaultValue="john@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="secret123" />
+        <button type="submit" className="btn btn-clock" disabled={isSubmitting}>
+          {isSubmitting ? "submitting..." : "submit"}
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
         </p>
-      </Form>  
+      </Form>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
